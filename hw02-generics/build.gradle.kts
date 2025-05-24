ad411786af3de0990f38eca9d94f9187e07df002
Валерия <lera.evstratova@gmail.com>
2025-05-24 17:01:08 +0300

    hw02-generics

---
+++ hw02-generics/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("org.assertj:assertj-core")
+}
