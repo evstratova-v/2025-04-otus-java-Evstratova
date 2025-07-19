879bfc3d7986e6f5a18e86d8e847a7c239e8e2e3
Валерия <lera.evstratova@gmail.com>
2025-07-19 16:58:07 +0300

    hw08-io

--- hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,35 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import ru.otus.model.Measurement;
 
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private final String fileName;
+
+    private final ObjectMapper mapper;
+
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+        this.mapper = new JsonMapper();
+    }
 
     @Override
     public List<Measurement> load() {
         // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        try (var inputStream = ResourcesFileLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            if (inputStream == null) {
+                throw new FileProcessException("Файл '%s' не найден".formatted(fileName));
+            }
+            byte[] bytes = inputStream.readAllBytes();
+            var measurementArrays = mapper.readValue(bytes, Measurement[].class);
+            return Arrays.asList(measurementArrays);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
