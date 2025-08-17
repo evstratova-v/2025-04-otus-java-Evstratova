93ecfa948ed5caa3d410bf0a914418023990a655
Валерия <lera.evstratova@gmail.com>
2025-08-18 02:26:41 +0300

    hw12-webServer

--- hw12-webServer/build.gradle.kts
+++ hw12-webServer/build.gradle.kts
@@ -8,4 +8,15 @@ dependencies {
 
     implementation("org.postgresql:postgresql")
     runtimeOnly("org.flywaydb:flyway-database-postgresql")
+
+    implementation("com.google.code.gson:gson")
+
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-servlet")
+    implementation("org.eclipse.jetty:jetty-server")
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-webapp")
+    implementation("org.eclipse.jetty:jetty-security")
+    implementation("org.eclipse.jetty:jetty-http")
+    implementation("org.eclipse.jetty:jetty-io")
+    implementation("org.eclipse.jetty:jetty-util")
+    implementation("org.freemarker:freemarker")
 }
